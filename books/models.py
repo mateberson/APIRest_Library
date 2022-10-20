2a94b194095e850a5fd37efd81a5ba5b303798fd
mateberson <mateus.pereira.r@gmail.com>
2022-10-19 21:55:49 -0300

    Inclusion of image in model books

--- books/models.py
+++ books/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 # Create your models here.
 
+def uploadImageBook(instance, filename):
+    return f'{instance.id_book}-{filename}'
+
 class Books(models.Model):
     id_book=models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title=models.CharField(max_length=255)
@@ -13,3 +16,4 @@ class Books(models.Model):
     pages=models.IntegerField()
     publshing_company=models.CharField(max_length=255)
     create_at=models.DateField(auto_now_add=True) # auto_now_add adiciona data ao ser inserido no banco
+    image=models.ImageField(upload_to=uploadImageBook, blank=True, null=True)
